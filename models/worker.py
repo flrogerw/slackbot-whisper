2044160496db1dcb8f18bb36c4d52c0768919a89
Roger Williams <roger.williams@audacy.com>
2025-02-13 12:09:40 -0700

    Debugging

--- models/worker.py
+++ models/worker.py
@@ -49,11 +49,10 @@ from queue import Queue
 from tempfile import NamedTemporaryFile
 
 import requests
-# import torch
 import whisper
 from dateutil.tz import tz
 from dotenv import load_dotenv
-from transformers import pipeline
+
 
 from models.gemini_model import GeminiQuery
 from models.google_doc_model import GoogleDocsManager
@@ -79,8 +78,6 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # Add missing mimetypes
 mimetypes.add_type('audio/vnd.wave', '.wav')
-
-summarizer = pipeline("summarization")
 
 
 class Worker(multiprocessing.Process):
@@ -387,7 +384,7 @@ class Worker(multiprocessing.Process):
             logging.info(f"Split into paragraphs: {datetime.now() - start_time}")
             paragraphs = Paragraphs(model_response['text'])
             paragraphs_list = paragraphs.get_paragraphs()
-            summary = summarizer(model_response['text'], max_length=50, min_length=10, do_sample=False)
+            summary = "Hello World"
 
             logging.info(f"Initialize the Google Docs manager: {datetime.now() - start_time}")
             # Initialize the Google Docs manager
